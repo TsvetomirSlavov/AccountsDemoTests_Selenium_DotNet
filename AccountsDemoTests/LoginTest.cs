e12e4f404e9f583b626facf2d4ffd748ff5b4eb7
Pani Kumar <kumarpani@gmail.com>
2014-07-18 21:24:43 +0530

    Chapter1: Adding First Test

---
+++ AccountsDemoTests/LoginTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace AccountsDemoTests
+{
+    [TestFixture]
+    public class LoginTest
+    {
+        [Test]
+        public void UserCanLoginSuccessfully()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("test-type");
+
+            IWebDriver driver = driver = new ChromeDriver(options);
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("http://accountsdemo.herokuapp.com/");
+            driver.FindElement(By.Id("user_email")).SendKeys("account1@ad.com");
+            driver.FindElement(By.Id("user_password")).SendKeys("password");
+            driver.FindElement(By.Name("commit")).Click();
+            Assert.True(driver.FindElement(By.CssSelector(".header>div>h2")).Text.StartsWith("Harry"),
+                "Signed in User name did not match");
+            driver.Quit();
+        }
+    }
+}
---
+++ AccountsDemoTests/NUnitConsoleRunner.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountsDemoTests
+{
+    public class NUnitConsoleRunner
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            NUnit.ConsoleRunner.Runner.Main(args);
+        }
+
+      
+    }
+}
