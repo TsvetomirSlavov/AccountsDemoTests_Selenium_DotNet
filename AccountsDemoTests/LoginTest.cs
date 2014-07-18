b005ad8a022ac5e329f5524eaddc39bc84d81503
Pani Kumar <kumarpani@gmail.com>
2014-07-18 21:31:55 +0530

    Chapter2: Adding Second Test

--- AccountsDemoTests/LoginTest.cs
+++ AccountsDemoTests/LoginTest.cs
@@ -22,8 +22,30 @@ namespace AccountsDemoTests
             driver.FindElement(By.Id("user_email")).SendKeys("account1@ad.com");
             driver.FindElement(By.Id("user_password")).SendKeys("password");
             driver.FindElement(By.Name("commit")).Click();
+
             Assert.True(driver.FindElement(By.CssSelector(".header>div>h2")).Text.StartsWith("Harry"),
                 "Signed in User name did not match");
+            
+            driver.Quit();
+        }
+
+        [Test]
+        public void UserShouldBeForcedToLoginWhenAccessesAuthenticatedPages()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("test-type");
+
+            IWebDriver driver = driver = new ChromeDriver(options);
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("http://accountsdemo.herokuapp.com/clients");
+            driver.FindElement(By.Id("user_email")).SendKeys("account1@ad.com");
+            driver.FindElement(By.Id("user_password")).SendKeys("password");
+            driver.FindElement(By.Name("commit")).Click();
+            
+            Assert.AreEqual("http://accountsdemo.herokuapp.com/clients", driver.Url.ToString(),
+                "User should have been landed in Clients Page");
+            
             driver.Quit();
         }
     }
